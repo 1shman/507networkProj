f8ee4e3b091a6b7e04592cda42c0f6b08599af6d
Ishraq Rahman <ishman@umich.edu>
2025-04-14 22:51:47 -0400

    add rough outline for high level overview

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def main():
     print(nba.head())
 
     # Option 1 Return a list of the most connected colleges - gives insights to the college with highest draft history (tie between MSU/UM)
-    # Option 2 
+        # Give option to visualize
+    # Option 2 For each college - NBA team, check their calculated composite score to give insights on athlete performance
+        # Give option to visualize
+    # Option 3 Return a list of colleges that are most historically drafted by an inputted NBA team
+        # Give option to visualize
 
 if __name__ == "__main__":
     main()
