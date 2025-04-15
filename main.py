1d55132832c950473a6be0bae5d7317e1613c068
Ishraq Rahman <ishman@umich.edu>
2025-04-14 22:45:08 -0400

    composite score

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ def main():
     nba = pd.read_csv("all_seasons.csv")
     print(nba.head())
 
+    # Option 1 Return a list of the most connected colleges - gives insights to the college with highest draft history (tie between MSU/UM)
+    # Option 2 
+
 if __name__ == "__main__":
     main()
